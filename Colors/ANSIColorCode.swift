9ce2e94264830eff8cfd1b89c374e77638a8e3c9
Paulo Tanaka <ptanaka@linkedin.com>
2015-12-14 23:56:42 -0800

    Adding Color code stuff

--- Colors/ANSIColorCode.swift
+++ Colors/ANSIColorCode.swift
@@ -1,19 +1,11 @@
-//
-//  ANSIBgColorCode.swift
-//  Colors
-//
-//  Created by Paulo Tanaka on 12/12/15.
-//  Copyright © 2015 Paulo Tanaka. All rights reserved.
-//
-
 // +30 for text +40 for bg + 90 for btext +100 for bgb
 struct ANSIColorCode {
-    static var black = [0, 9]
-    static var red = [1, 9]
-    static var green = [2, 9]
-    static var yellow = [3, 9]
-    static var blue = [4, 9]
-    static var magenta = [5, 9]
-    static var cyan = [6, 9]
-    static var white = [7, 9]
+    static let black = [0, 9]
+    static let red = [1, 9]
+    static let green = [2, 9]
+    static let yellow = [3, 9]
+    static let blue = [4, 9]
+    static let magenta = [5, 9]
+    static let cyan = [6, 9]
+    static let white = [7, 9]
 }
---
+++ Colors/ANSIProperties.swift
@@ -0,0 +1,9 @@
+//
+//  ANSIProperties.swift
+//  Colors
+//
+//  Created by Paulo Tanaka on 12/13/15.
+//  Copyright © 2015 Paulo Tanaka. All rights reserved.
+//
+
+import Foundation
